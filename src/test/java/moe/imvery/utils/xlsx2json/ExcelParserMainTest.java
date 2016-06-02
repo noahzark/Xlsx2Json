9eb5d8471b7415847faebdf3147f610095da542f
Feliciano Long <admin@fz-l.com>
2016-06-02 18:15:17 -0500

    Add some more test cases

---
+++ src/test/java/moe/imvery/utils/xlsx2json/ExcelParserMainTest.java
@@ -0,0 +1,88 @@
+package moe.imvery.utils.xlsx2json;
+
+import org.junit.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
+
+import java.io.BufferedReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Feliciano on 6/2/2016.
+ */
+public class ExcelParserMainTest {
+
+    @Test
+    public void main() throws Exception {
+        ExcelParserMain.main(new String[]{"test.xlsx", "map", "true"});
+
+        Path path = Paths.get("test.json");
+        BufferedReader reader = Files.newBufferedReader(path);
+        String jsonText = reader.readLine();
+
+        path = Paths.get("test3.expected.json");
+        reader = Files.newBufferedReader(path);
+        String expected = reader.readLine();
+
+        JSONAssert.assertEquals(expected, jsonText, false);
+    }
+
+    @Test
+    public void mainMultisheets() throws Exception {
+        ExcelParserMain.main(new String[]{"test.xlsx", "weaponStuffs skillStuffs essenceStuffs", "true"});
+
+        Path path = Paths.get("test.json");
+        BufferedReader reader = Files.newBufferedReader(path);
+        String jsonText = reader.readLine();
+
+        path = Paths.get("test4.expected.json");
+        reader = Files.newBufferedReader(path);
+        String expected = reader.readLine();
+
+        JSONAssert.assertEquals(expected, jsonText, false);
+    }
+
+    @Test
+    public void mainHideSheetName() throws Exception {
+        ExcelParserMain.main(new String[]{"test.xlsx", "map", "false"});
+
+        Path path = Paths.get("test.json");
+        BufferedReader reader = Files.newBufferedReader(path);
+        String jsonText = reader.readLine();
+
+        path = Paths.get("test5.expected.json");
+        reader = Files.newBufferedReader(path);
+        String expected = reader.readLine();
+
+        JSONAssert.assertEquals(expected, jsonText, false);
+    }
+
+    @Test
+    public void mainMultiSheetsAndHideSheetName() throws Exception {
+        ExcelParserMain.main(new String[]{"test.xlsx", "map monster", "false"});
+
+        Path path = Paths.get("test.json");
+        BufferedReader reader = Files.newBufferedReader(path);
+        String jsonText = reader.readLine();
+
+        path = Paths.get("test6.expected.json");
+        reader = Files.newBufferedReader(path);
+        String expected = reader.readLine();
+
+        JSONAssert.assertEquals(expected, jsonText, false);
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void mainWrongTargetName() throws Exception {
+        ExcelParserMain.main(new String[]{"test.xls", "", ""});
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void mainWrongNumberofArguments() throws Exception {
+        ExcelParserMain.main(new String[]{"test.xls"});
+    }
+
+}
