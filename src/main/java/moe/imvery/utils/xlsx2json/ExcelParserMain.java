35d7c59fcfbf8d93d4bdf98e968d018c6aa6c7c0
Feliciano Long <admin@fz-l.com>
2016-06-02 18:14:54 -0500

    Complete Main class functions

--- src/main/java/moe/imvery/utils/xlsx2json/ExcelParserMain.java
+++ src/main/java/moe/imvery/utils/xlsx2json/ExcelParserMain.java
@@ -18,23 +18,50 @@ import java.nio.file.Paths;
  */
 public class ExcelParserMain {
 
-    public static void main(String[] args) {
-        String fileName = "test";
-        File excelFile = new File(fileName + ".xlsx");
+    /**
+     * Parse the excel file and save as json
+     * @param targetName Excel file name without suffix
+     * @param sheetList Target sheet list
+     * @param showSheetName Whether show sheet name in result or not
+     */
+    public static void parseExcel(String targetName, String[] sheetList, boolean showSheetName) {
+        File excelFile = new File(targetName + ".xlsx");
 
-        try(FileInputStream inp = new FileInputStream( excelFile )) {
+        try {
+            FileInputStream inp = new FileInputStream( excelFile );
             Workbook workbook = WorkbookFactory.create( inp );
-            // Start constructing JSON.
-            JSONObject json = new JSONObject();
 
-            // Create JSON
-            String configName = "shieldStuffs";
-            JSONArray rows = ExcelParser.parseSheet(workbook, configName);
-            json.put( configName, rows );
+            String jsonText;
 
-            // Get the JSON text.
-            String jsonText = json.toString();
-            Path path = Paths.get(fileName + ".json");
+            if (showSheetName) {
+                // Start constructing JSON.
+                JSONObject json = new JSONObject();
+
+                // Create JSON
+                for (String sheetName : sheetList) {
+                    JSONArray rows = ExcelParser.parseSheet(workbook, sheetName);
+                    json.put(sheetName, rows);
+                }
+
+                // Get the JSON text.
+                jsonText = json.toString();
+            } else {
+                JSONArray json = new JSONArray();
+
+                // Create JSON
+                for (String sheetName : sheetList) {
+                    JSONArray rows = ExcelParser.parseSheet(workbook, sheetName);
+                    for (int i=0; i < rows.length(); i++) {
+                        json.put(rows.get(i));
+                    }
+                }
+
+                // Get the JSON text.
+                jsonText = json.toString();
+            }
+
+            // Write into file
+            Path path = Paths.get(targetName + ".json");
             BufferedWriter writer = Files.newBufferedWriter( path );
             writer.write(jsonText);
             writer.close();
@@ -43,6 +70,28 @@ public class ExcelParserMain {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static void main(String[] args) {
+        if (args.length < 2)
+            throw new IllegalArgumentException("Expected at least 2 arguments representing Filename, Sheetnames(Divided by space, surrounded by \").");
+
+        if (args.length > 3)
+            throw new IllegalArgumentException("Expected at most 3 arguments representing Filename, Sheetnames(Divided by space, surrounded by \") and a boolean for show sheet names in result or not.");
+
+        String targetName = args[0];
+        if (!targetName.endsWith("xlsx"))
+            throw new IllegalArgumentException("The first argument should be a excel(xlsx) file name.");
+        // Cut the .xlsx suffix
+        targetName = targetName.substring(0, targetName.length()-5);
+
+        // Split sheet names
+        String[] sheetList = args[1].split(" ");
+
+        // Detect show sheet name option
+        boolean showSheetName = (args.length == 3) ? Boolean.parseBoolean(args[2]) : false;
+
+        parseExcel(targetName, sheetList, showSheetName);
     }
 
 }
