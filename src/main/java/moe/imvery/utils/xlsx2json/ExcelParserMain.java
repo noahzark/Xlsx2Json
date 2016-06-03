fd04e2c893b59e1ddada68840d09d2de6a272c78
Feliciano Long <me@imvery.moe>
2016-06-03 14:46:25 -0500

    Add json compare function and update test cases

--- src/main/java/moe/imvery/utils/xlsx2json/ExcelParserMain.java
+++ src/main/java/moe/imvery/utils/xlsx2json/ExcelParserMain.java
@@ -3,12 +3,11 @@ package moe.imvery.utils.xlsx2json;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.*;
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+import org.skyscreamer.jsonassert.JSONAssert;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -72,6 +71,32 @@ public class ExcelParserMain {
         }
     }
 
+    /**
+     * Validate two json file.  If it isn't it throws an {@link AssertionError}.
+     * @param expectedFileName Expected JSON file
+     * @param targetFileName File to compare
+     * @param strict Enables strict checking
+     * @throws JSONException
+     */
+    public static void validateJson(String expectedFileName, String targetFileName, boolean strict) {
+        try {
+            System.out.println("Checking " + targetFileName + " ... ");
+            Path path = Paths.get(targetFileName);
+            BufferedReader reader = Files.newBufferedReader(path);
+            String jsonText = reader.readLine();
+
+            path = Paths.get(expectedFileName);
+            reader = Files.newBufferedReader(path);
+            String expected = reader.readLine();
+
+            JSONAssert.assertEquals(expected, jsonText, false);
+
+            System.out.println("Passed.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
         if (args.length < 2)
             throw new IllegalArgumentException("Expected at least 2 arguments representing Filename, Sheetnames(Divided by space, surrounded by \").");
@@ -80,18 +105,23 @@ public class ExcelParserMain {
             throw new IllegalArgumentException("Expected at most 3 arguments representing Filename, Sheetnames(Divided by space, surrounded by \") and a boolean for show sheet names in result or not.");
 
         String targetName = args[0];
-        if (!targetName.endsWith("xlsx"))
-            throw new IllegalArgumentException("The first argument should be a excel(xlsx) file name.");
-        // Cut the .xlsx suffix
-        targetName = targetName.substring(0, targetName.length()-5);
 
-        // Split sheet names
-        String[] sheetList = args[1].split(" ");
+        if (targetName.equalsIgnoreCase("Validate")) {
 
-        // Detect show sheet name option
-        boolean showSheetName = (args.length == 3) ? Boolean.parseBoolean(args[2]) : false;
+        } else {
+            if (!targetName.endsWith("xlsx"))
+                throw new IllegalArgumentException("The first argument should be a excel(xlsx) file name.");
+            // Cut the .xlsx suffix
+            targetName = targetName.substring(0, targetName.length()-5);
 
-        parseExcel(targetName, sheetList, showSheetName);
+            // Split sheet names
+            String[] sheetList = args[1].split(" ");
+
+            // Detect show sheet name option
+            boolean showSheetName = (args.length == 3) ? Boolean.parseBoolean(args[2]) : false;
+
+            parseExcel(targetName, sheetList, showSheetName);
+        }
     }
 
 }
--- src/test/java/moe/imvery/utils/xlsx2json/ExcelParserMainTest.java
+++ src/test/java/moe/imvery/utils/xlsx2json/ExcelParserMainTest.java
@@ -19,60 +19,28 @@ public class ExcelParserMainTest {
     public void main() throws Exception {
         ExcelParserMain.main(new String[]{"testcases/test.xlsx", "map", "true"});
 
-        Path path = Paths.get("testcases/test.json");
-        BufferedReader reader = Files.newBufferedReader(path);
-        String jsonText = reader.readLine();
-
-        path = Paths.get("testcases/test3.expected.json");
-        reader = Files.newBufferedReader(path);
-        String expected = reader.readLine();
-
-        JSONAssert.assertEquals(expected, jsonText, false);
+        ExcelParserMain.validateJson("testcases/test3.expected.json", "testcases/test.json", false);
     }
 
     @Test
     public void mainMultisheets() throws Exception {
         ExcelParserMain.main(new String[]{"testcases/test.xlsx", "weaponStuffs skillStuffs essenceStuffs", "true"});
 
-        Path path = Paths.get("testcases/test.json");
-        BufferedReader reader = Files.newBufferedReader(path);
-        String jsonText = reader.readLine();
-
-        path = Paths.get("testcases/test4.expected.json");
-        reader = Files.newBufferedReader(path);
-        String expected = reader.readLine();
-
-        JSONAssert.assertEquals(expected, jsonText, false);
+        ExcelParserMain.validateJson("testcases/test4.expected.json", "testcases/test.json", false);
     }
 
     @Test
     public void mainHideSheetName() throws Exception {
         ExcelParserMain.main(new String[]{"testcases/test.xlsx", "map", "false"});
 
-        Path path = Paths.get("testcases/test.json");
-        BufferedReader reader = Files.newBufferedReader(path);
-        String jsonText = reader.readLine();
-
-        path = Paths.get("testcases/test5.expected.json");
-        reader = Files.newBufferedReader(path);
-        String expected = reader.readLine();
-
-        JSONAssert.assertEquals(expected, jsonText, false);
+        ExcelParserMain.validateJson("testcases/test5.expected.json", "testcases/test.json", false);
     }
 
     @Test
     public void mainMultiSheetsAndHideSheetName() throws Exception {
         ExcelParserMain.main(new String[]{"testcases/test.xlsx", "map monster", "false"});
 
-        Path path = Paths.get("testcases/test.json");
-        BufferedReader reader = Files.newBufferedReader(path);
-        String jsonText = reader.readLine();
-
-        path = Paths.get("testcases/test6.expected.json");
-        reader = Files.newBufferedReader(path);
-        String expected = reader.readLine();
-
-        JSONAssert.assertEquals(expected, jsonText, false);
+        ExcelParserMain.validateJson("testcases/test6.expected.json", "testcases/test.json", false);
     }
 
     @Test(expected=IllegalArgumentException.class)
